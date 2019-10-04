6fb89c5c2bb4c7571f9796effac2cd9d1356920c
Luis Garcia <lgarcia@sintad.pe>
2019-10-04 18:03:06 -0500

    Starter Kit AdminLTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { ContentComponent } from './components/shared/content/content.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SettingComponent } from './components/shared/setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContentComponent,
+    FooterComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
